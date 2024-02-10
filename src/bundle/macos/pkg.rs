34d86c7687c2c597727fdd08973ae77241c40f67
masato yuna <masato.yuna1@gmail.com>
2024-02-10 19:20:16 +0900

    update

--- src/bundle/macos/pkg.rs
+++ src/bundle/macos/pkg.rs
@@ -92,7 +92,14 @@ pub fn bundle_project(settings: &Settings, bundles: &[Bundle]) -> crate::Result<
     .stderr(Stdio::piped())
     .output()
     .expect("Failed to chmod script");
-
+  Command::new("chmod")
+    .arg("777")
+    .arg(&bundle_dir.join("*"))
+    .current_dir(&output_path)
+    .stdout(Stdio::piped())
+    .stderr(Stdio::piped())
+    .output()
+    .expect("Failed to chmod macos app bundle script");
     
   let mut args = vec![
     bundle_dir.to_string_lossy().to_string(),
